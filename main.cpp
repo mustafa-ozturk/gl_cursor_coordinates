f6d81cb634b7efe9dbc5a32e5ae89b910597325b
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-31 06:57:19 -0400

    setup code for window and opengl

--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <glbinding/glbinding.h>
+#include <glbinding/gl/gl.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "gl_textrenderer/gl_textrenderer.h"
+#include "gl_gridlines/gl_gridlines.h"
+
+using namespace gl;
+
+const unsigned int SCREEN_WIDTH = 500;
+const unsigned int SCREEN_HEIGHT = 500;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
+                                          "gl_cursor_coordinates", nullptr,
+                                          nullptr);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glbinding::initialize(glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
 }
